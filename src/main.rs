2db147b8cd483c703f94e1e09aa2fa7288581fe9
Logan King <seanloganking@gmail.com>
2022-03-24 21:44:49 -0400

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 use std::fs::File;
+use std::io::Write;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-fn add_whitespace(line: &str, tab_depth: u32, tab_spaces: u32) -> String{
-
+fn add_whitespace(line: &str, tab_depth: u32, tab_spaces: u32) -> String {
     let mut value = String::from("");
 
-    for _i in 0..tab_depth*tab_spaces{
+    for _i in 0..tab_depth * tab_spaces {
         value.push(' ');
     }
 
@@ -22,25 +24,19 @@ fn add_whitespace(line: &str, tab_depth: u32, tab_spaces: u32) -> String{
 }
 
 fn main() {
-    // println!("Hello, world!");
-
     let mut formatted_file = String::from("");
 
     let tab_spaces = 4;
     let mut current_tab_depth = 0;
 
-    
-
     if let Ok(lines) = read_lines("./input/formatted_lib.rs") {
-
         for line in lines {
-
             let line = line.expect("Line not valid");
             let mut starting_chars = String::from("");
 
             let char_vec: Vec<char> = line.chars().collect();
-            for (i, char) in char_vec.iter().enumerate(){
-                if *char as u32 > 32{
+            for (i, char) in char_vec.iter().enumerate() {
+                if *char as u32 > 32 {
                     starting_chars = String::from(&line[i..]);
                     break;
                 }
@@ -48,24 +44,19 @@ fn main() {
 
             let formatted_line;
 
-            // println!("starting_chars: {}", starting_chars);
-            // println!("current_tab_depth: {}",current_tab_depth);
-
-            if starting_chars.starts_with("//>"){
+            if starting_chars.starts_with("//>") {
                 formatted_line = add_whitespace(&line, current_tab_depth, tab_spaces);
                 current_tab_depth += 1;
-            } else if starting_chars.starts_with("//<>"){
+            } else if starting_chars.starts_with("//<>") {
                 current_tab_depth -= 1;
                 formatted_line = add_whitespace(&line, current_tab_depth, tab_spaces);
                 current_tab_depth += 1;
-            } else if starting_chars.starts_with("//<"){
+            } else if starting_chars.starts_with("//<") {
                 current_tab_depth -= 1;
                 formatted_line = add_whitespace(&line, current_tab_depth, tab_spaces);
-            }else{
+            } else {
                 formatted_line = add_whitespace(&line, current_tab_depth, tab_spaces);
             }
-
-            // println!("{}",formatted_line);
 
             formatted_file.push_str(&(formatted_line + "\n"));
         }
@@ -74,14 +65,13 @@ fn main() {
     // remove last \n
     formatted_file.pop();
 
-    println!("{}",formatted_file);
+    println!("{}", formatted_file);
 
-    // use std::fs::File;
-    use std::io::{Write};
+    //> write file
+        let path = "./input/results.rs";
+        let mut output = File::create(path).unwrap();
+        write!(output, "{}", formatted_file).expect("failed to write file");
+    //<
 
-    let path = "./input/results.rs";
-    let mut output = File::create(path).unwrap();
-    write!(output, "{}", formatted_file).expect("failed to write file");
-
-    // println!("{}",add_whitespace("Hello World!", 5, 4));
+    assert!(current_tab_depth == 0, "unclosed comment");
 }
